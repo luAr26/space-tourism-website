98ba39700683ffc428c65cfa80208eb2a8cc0779
luAr26 <raul_cosmin_26@yahoo.com>
2024-02-11 13:09:40 +0200

    Complete home page partially 2.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const navBtn = document.querySelector('.mobile-nav-toggle');
+
+navBtn.addEventListener('click', function() {
+  document.body.classList.toggle('jsOpenNav');
+  const ariaExpandedAttr = this.getAttribute('aria-expanded');
+  if (ariaExpandedAttr === 'false') {
+    this.setAttribute('aria-expanded', 'true')
+  } else {
+    this.setAttribute('aria-expanded', 'false');
+  }  
+})
+
