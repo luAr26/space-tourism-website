c001073a3db9e39857473d39569b8e98a1ac004e
luAr26 <raul_cosmin_26@yahoo.com>
2024-02-20 21:50:52 +0200

    Destination page completed.

--- js/main.js
+++ js/main.js
@@ -31,40 +31,50 @@ function changeTabFocus(e) {
   if (e.keyCode === keydownRight || e.keyCode === keydownLeft) {
     tabs[tabFocus].setAttribute('tabindex', '-1');
     // tabs[tabFocus].setAttribute('aria-selected', 'false');
+      if(e.keyCode === keydownRight) {
+      tabFocus++;
+      if (tabFocus >= tabs.length) {
+        tabFocus = 0;
+      }
+    };
+
+    if (e.keyCode === keydownLeft) {
+      tabFocus--;
+      if (tabFocus < 0) {
+        tabFocus = tabs.length - 1;
+      }
+    }
+
+    tabs[tabFocus].focus();
+    tabs[tabFocus].setAttribute('tabindex', "0");
   }
   
-  if(e.keyCode === keydownRight) {
-    tabFocus++;
-    if (tabFocus >= tabs.length) {
-      tabFocus = 0;
-    }
-  };
-
-  if (e.keyCode === keydownLeft) {
-    tabFocus--;
-    if (tabFocus < 0) {
-      tabFocus = tabs.length - 1;
-    }
-  }
-
-  tabs[tabFocus].focus();
-  tabs[tabFocus].setAttribute('tabindex', "0");
   
+  
+}
+
+function hideContent(parent, content) {
+  parent.querySelectorAll(content).forEach(item => item.hidden = true);
+}
+
+function showContent(parent, content) {
+  parent.querySelector(content).hidden = false;
 }
 
 function changeTabPanel(e) {
   const targetBtn = e.target;
   const targetPanel = targetBtn.getAttribute('aria-controls');
+  const targetImage = targetBtn.dataset.image;
   const tabContainer = targetBtn.parentNode;
   const mainContainer = tabContainer.parentNode;
-  mainContainer.querySelectorAll('[role="tabpanel"]').forEach(panel => panel.hidden = true);
-  mainContainer.querySelector([`#${targetPanel}`]).hidden = false;
-  console.log(mainContainer);
   
-  // tabs.forEach(tab => tab.setAttribute('aria-selected', 'false'));
-  // targetBtn.setAttribute('aria-selected', 'true');
-  // console.log(tabs[tabFocus].getAttribute('aria-controls'));
-  // const selectedArticle = document.getElementById(targetBtn.getAttribute('aria-controls'));
-  // destinationInfoArticles.forEach(article => article.hidden = true);
-  // selectedArticle.hidden = false;
+  hideContent(mainContainer, '[role="tabpanel"]');
+  showContent(mainContainer, [`#${targetPanel}`]);
+  
+  hideContent(mainContainer, 'picture');
+  showContent(mainContainer, [`#${targetImage}`]);
+
+  tabContainer.querySelector('[aria-selected="true"]').setAttribute('aria-selected', false);
+  targetBtn.setAttribute('aria-selected', true);
 }
+
