645fa4a0d7c4f87444b7ed56a3384f2bd9664452
luAr26 <raul_cosmin_26@yahoo.com>
2024-02-22 21:12:16 +0200

    Complete the crew page.

--- js/tabs.js
+++ js/tabs.js
@@ -1,18 +1,4 @@
-// NAVIGATION
-const navBtn = document.querySelector('.mobile-nav-toggle');
-
-navBtn.addEventListener('click', function() {
-  document.body.classList.toggle('jsOpenNav');
-  const ariaExpandedAttr = this.getAttribute('aria-expanded');
-  if (ariaExpandedAttr === 'false') {
-    this.setAttribute('aria-expanded', 'true')
-  } else {
-    this.setAttribute('aria-expanded', 'false');
-  }  
-})
-
 // TABS
-
 const tabList = document.querySelector('[role="tablist"');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 // const destinationInfoArticles = document.querySelectorAll('.destination-info');
